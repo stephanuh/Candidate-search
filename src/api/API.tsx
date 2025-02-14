cf01433e058f3bc8543f45c64cb6be2d20a5c802
Papaya <stephaniesosa02@gmail.com>
2025-02-14 13:56:03 -0700

    added folders and code

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -1,4 +1,5 @@
-const searchGithub = async () => {
+import Candidate from "../interfaces/Candidate.interface";
+const searchGithub = async (): Promise<{ login:string}[]> => {
   try {
     const start = Math.floor(Math.random() * 100000000) + 1;
     // console.log(import.meta.env);
@@ -11,7 +12,10 @@ const searchGithub = async () => {
       }
     );
     // console.log('Response:', response);
-    const data = await response.json();
+    const data: {login: string }[] = (await response.json()) as {
+      login: string;
+    }[];
+
     if (!response.ok) {
       throw new Error('invalid API response, check the network tab');
     }
@@ -23,21 +27,30 @@ const searchGithub = async () => {
   }
 };
 
-const searchGithubUser = async (username: string) => {
+const searchGithubUser = async (username: string): Promise<Candidate> => {
   try {
     const response = await fetch(`https://api.github.com/users/${username}`, {
       headers: {
         Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
       },
     });
-    const data = await response.json();
+    const data: Candidate = (await response.json()) as Candidate;
     if (!response.ok) {
       throw new Error('invalid API response, check the network tab');
     }
     return data;
   } catch (err) {
     // console.log('an error occurred', err);
-    return {};
+    return {
+      name: "",
+      login: "",
+      location: "",
+      email: "",
+      company: "",
+      bio: "",
+      html_url: "",
+      avatar_url: "",
+    };
   }
 };
 
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,28 @@
+import { Link, useLocation } from "react-router-dom";
+
 const Nav = () => {
+  const currentPage = useLocation().pathname;
+
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <h2>
+            <Link to="/" className={currentPage === "/" ? "nav-link active " : "nav-link" }>
+            Home
+            </Link>
+          </h2>
+        </li>
+        <li className="nav-item">
+          <h2>
+            <Link to="SavedCandidates" className={currentPage === "SavedCandidates" ? "nav-link active " : "nav-link" }>
+            Potential Candidates
+            </Link>
+          </h2>
+        </li>
+      </ul>
+    </div>
   )
 };
 
