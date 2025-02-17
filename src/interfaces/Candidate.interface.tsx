2e2c57679ca4b54b72b6997c1aa1ef470e05cd51
Papaya <stephaniesosa02@gmail.com>
2025-02-16 18:59:36 -0700

    fixed bug

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,14 +1,14 @@
 // TODO: Create an interface for the Candidate objects returned by the API
 
 interface Candidate {
+    avatar_url: string | null;
     name: string | null;
     login: string | null;
     location: string | null;
     email: string | null;
     company: string | null;
-    bio: string | null;
     html_url: string | null;
-    avatar_url: string | null;
+    bio: string | null;
 }
 
 export default Candidate;
